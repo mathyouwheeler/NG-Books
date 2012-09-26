6ce943b5486d50b4d430c83b8f7658489b6b28d9
Matt Wheeler <gooblegotz@gmail.com>
2012-09-26 17:18:00 -0400

    first major commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NgBooks::Application.config.secret_token = '119daa11725599adb27587a1a6f4e6c28724aa28aaf49bdaa2c24a9be8a4080b4c1c78f5de531ec864235bc5d863c454a14c75410af4d6cfffeaff7a3bc57911'
